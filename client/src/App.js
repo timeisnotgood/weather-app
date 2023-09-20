752cb5f71c82417b55ed7d59d9d93cb6bc78de2d
timeisnotgood <bhoopathicl@gmail.com>
2023-09-20 18:44:19 +0530

    Firebase Auth

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,27 @@
+import React, { useState } from 'react'
 
-function App() {
+const App = () => {
+  const [data, setdata] = useState({
+    email : "",
+    password : ""
+  })
+
+  const handler = (e) =>{
+    setdata({...data, [e.target.name] : e.target.value})
+  }
+  const subhandler = (e) =>{
+    e.preventDefault();
+    console.log(data);
+  }
   return (
-    <div className="App">
-      <h3>Weather-App</h3>
+    <div>
+      <form onSubmit={subhandler} >
+        <input type='text' name='email' onChange={handler} placeholder='email'  />
+        <input type='text' name='password' onChange={handler} placeholder='password' />
+        <button>Submit</button>
+      </form>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
---
+++ server/index.js
@@ -0,0 +1,6 @@
+const express = require("express")
+const app = express()
+
+app.listen(5000, (req, res)=>{
+    console.log("Connected successfully");
+})
