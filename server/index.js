852f300bebb803470bc8f2ffaee451c1c541918a
timeisnotgood <bhoopathicl@gmail.com>
2023-10-16 18:42:05 +0530

    index

--- server/index.js
+++ server/index.js
@@ -1,19 +1,64 @@
 const express = require("express")
-const dotenv = require('dotenv')
-const cors = require('cors')
+
 const app = express()
-const db = require("./lib/db")
+const port = 5000
 
 
-const port = process.env.PORT || 5001
-app.use(cors())
 app.use(express.json())
-app.use('/user', require("./routes/routes"))
 
-db()
+app.get("/data", (req, res)=>{
+    res.json({message : "Get Method"})
+})
 
-if (db) {
-    app.listen(port, (req, res)=>{
-        console.log("Server Started !..", port);
-    })   
-}
+
+app.post("/data", (req, res)=>{
+    res.json({message : "Post Method"})
+})
+
+
+
+app.put("/data/:id", async(req, res)=>{
+    const insert = await use.findByIdAndUpdate(id,{
+        name,
+        email,
+        password
+    },{new : true})
+    res.status(200).json(insert)
+})
+
+app.delete("/data/:id", async(req, res)=>{
+    const delet = await use.deleteOne({_id : id})
+    if(delet){
+        res.status(200).json({message:`The Deleted contact is ${req.params.id}`})
+    }
+})
+
+
+
+app.listen( port, (req, res)=>{
+    console.log("server started !...");
+})
+
+
+
+
+// const express = require("express")
+// const dotenv = require('dotenv')
+// const cors = require('cors')
+// const app = express()
+// const db = require("./lib/db")
+
+
+// const port = process.env.PORT || 5001
+// app.use(cors())
+// app.use(express.json())
+// app.use('/user', require("./routes/routes"))
+
+// db()
+
+// if (db) {
+//     app.listen(port, (req, res)=>{
+//         console.log("Server Started !..", port);
+//     })   
+// }
+
