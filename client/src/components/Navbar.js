34432b7e2bc4cbd80c6a363d74fb91841cc14cd9
timeisnotgood <bhoopathicl@gmail.com>
2023-11-02 16:43:47 +0530

    change

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -5,23 +5,14 @@ export const Navbar = () => {
 
   const [loggedin, setloggedin] = useState(false)
   
-    const data = localStorage.getItem('token')
 
-
-  if(localStorage.getItem('token')){
-    setloggedin(true)
-  }
-
-  // useEffect(()=>{
-  //   function setter(){
-  //     const data = localStorage.getItem('token')
-  //     if(data) setloggedin(true)
-  //     console.log(data);
-  //   }
-  //   setter()
-  // })
-
-  console.log(loggedin);
+  useEffect(()=>{
+    function setter(){
+      const data = localStorage.getItem('token')
+      console.log(data);
+    }
+    setter()
+  })
   
   return (
     <div>
