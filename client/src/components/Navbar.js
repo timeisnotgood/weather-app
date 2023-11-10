b1942d8ab921d1748326f745a1def2fba6d90658
timeisnotgood <bhoopathicl@gmail.com>
2023-11-10 08:46:20 +0530

    login update

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,43 +1,103 @@
-import React, { useEffect, useState } from 'react'
-import { Link } from 'react-router-dom'
+import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
 
 export const Navbar = () => {
+  const [user, setuser] = useState(null);
+  const [loggedin, setloggedin] = useState(false);
 
-  const [user, setuser] = useState()
-  const [loggedin, setloggedin] = useState(false)
-  
-  const token = localStorage.getItem('token')
- 
-  useEffect(()=>{
+  useEffect(() => {
+    const token = localStorage.getItem('token');
 
-    async function setter(){
-      console.log(token);
-      const base64Url = token.split('.')[1];
-      const base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
-      const payload = JSON.parse(atob(base64));
-      const currentuser = payload.user
-      setuser(currentuser)
-      if(currentuser) setloggedin(true)
-      console.log(user);
+    if (token) {
+      try {
+        const base64Url = token.split('.')[1];
+        const base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
+        const payload = JSON.parse(atob(base64));
+        const currentuser = payload.user;
+
+        setuser(currentuser);
+        setloggedin(true);
+      } catch (error) {
+        console.error('Error decoding token:', error);
+        setuser(null);
+        setloggedin(false);
+      }
+    } else {
+      setuser(null);
+      setloggedin(false);
     }
+  }, [localStorage.getItem('token')]);
 
-    setter();
-  },[]);
+  const logouthandler = () => {
+    localStorage.removeItem('token');
+    setuser(null);
+    setloggedin(false);
+  };
 
-  const logouthandler = () =>{
-    localStorage.removeItem('token')
-    setloggedin(false)
-  }
+  console.log(user);
 
-  
   return (
     <div>
-        <Link to={"/"} >Home {user && user.name} </Link>
-        {token ? <button onClick={logouthandler} >Logout</button> 
-        :<div>
-          <Link to={"/register"} >Register</Link>
-          <Link to={"/login"} >Login</Link>
-        </div>}
+      <div >Home {user && user.username}</div>
+      {loggedin ? (
+        <button onClick={logouthandler}>Logout</button>
+      ) : (
+        <div>
+          <Link to="/register">Register</Link>
+          <Link to="/login">Login</Link>
+        </div>
+      )}
     </div>
-  )
-}
+  );
+};
+
+
+
+
+
+
+
+// import React, { useEffect, useState } from 'react'
+// import { Link } from 'react-router-dom'
+
+// export const Navbar = () => {
+
+//   const [user, setuser] = useState(null)
+//   const [loggedin, setloggedin] = useState(false)
+  
+//   const token = localStorage.getItem('token')
+ 
+//   useEffect(()=>{
+
+//     async function setter(){
+//       console.log(token);
+//       const base64Url = token.split('.')[1];
+//       const base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
+//       const payload = JSON.parse(atob(base64));
+//       const currentuser = payload.user
+//       setuser(currentuser)
+//       if(currentuser) setloggedin(true)
+//       console.log(user);
+//     }
+
+//     setter();
+//   },[localStorage.getItem('token')]);
+
+//   const logouthandler = () =>{
+//     localStorage.removeItem('token')
+//     setloggedin(false)
+//     setuser(null)
+//   }
+
+  
+//   return (
+//     <div>
+//         <Link to={"/"} >Home {user && user.name} </Link>
+//         {token ? <button onClick={logouthandler} >Logout</button> 
+//         :<div>
+//           <Link to={"/register"} >Register</Link>
+//           <Link to={"/login"} >Login</Link>
+//         </div>}
+//     </div>
+//   )
+// }
