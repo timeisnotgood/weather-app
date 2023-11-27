6f02221a0d31afe402501516641ed27a9f2eeb1a
timeisnotgood <bhoopathicl@gmail.com>
2023-11-27 12:13:43 +0530

    update and delete API completed

--- server/controller/todocontroller.js
+++ server/controller/todocontroller.js
@@ -1,12 +1,21 @@
 const asynchandler = require("express-async-handler")
 const todo = require("../model/todomodule")
 
+// get to-do
+// route /todo/get:id
+// private route
+
 const gettodo = asynchandler(async(req, res) =>{
     const id = req.params.id
     const user_id = id
     const get = await todo.find({user_id})
     res.send(get)
 })
+
+// create to-do
+// route /todo/post:id
+// private route
+
 
 const createtodo = asynchandler(async(req, res)=>{
     const id = req.params.id
@@ -18,8 +27,27 @@ const createtodo = asynchandler(async(req, res)=>{
     })
 
     res.send(create)
-    console.log(id, data.todo);
+})
+
+// get to-do
+// route /todo/update:id
+// private route
+
+const updatetodo = asynchandler(async(req, res)=>{
+    const id = req.params.id
+    const data = req.body
+
+    const update = await todo.updateOne({_id : id},{todo : data.todo})
+    res.send(update)
+})
+
+const deletetodo = asynchandler(async(req, res)=>{
+    const id = req.params.id
+    
+    const remove = await todo.deleteOne({_id : id})
+    res.send(remove)
 })
 
 
-module.exports = { gettodo, createtodo }
+
+module.exports = { gettodo, createtodo, updatetodo, deletetodo }
--- server/routes/todorouter.js
+++ server/routes/todorouter.js
@@ -1,13 +1,13 @@
 const express = require("express")
 const bodyParser = require("body-parser")
-const {gettodo, createtodo} = require("../controller/todocontroller")
+const {gettodo, createtodo, updatetodo, deletetodo} = require("../controller/todocontroller")
 const router = express.Router()
 
 
 router.use(bodyParser.json());
 router.get('/get/:id', gettodo)
 router.post('/create/:id', createtodo)
-// router.put()
-// router.delete()
+router.put('/update/:id', updatetodo)
+router.delete('/delete/:id', deletetodo)
 
 module.exports = router
